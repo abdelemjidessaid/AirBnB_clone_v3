b9b3c073aff7c7de95acb0b89c844033f8b77349
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-10-27 18:34:42 +0100

    conflict solved, all files updated.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 '''Initializes the views module'''
 from flask import Blueprint
 
@@ -15,22 +14,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.states import *
 from api.v1.views.users import *
-=======
-'''
-Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
-'''
-
-
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
->>>>>>> 9427253621f0c28c708069584672a8a645cabf58
